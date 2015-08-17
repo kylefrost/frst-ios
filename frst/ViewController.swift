6624c687f6fed5da2e880be2d312153d68c1704f
Kyle Frost <kylefrost@comcast.net>
2015-08-17 14:03:52 -0400

    Add the stuffs

--- frst/ViewController.swift
+++ frst/ViewController.swift
@@ -7,19 +7,32 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let parameters = [
+            "url": "http://www.thisisfromiossim.com/",
+            "alias": "",
+            "password": "this is kyle.1"
+        ]
+        
+        Alamofire.request(.POST, "http://frst.xyz/api/create", parameters: parameters).response {
+            request, response, data, error in
+            print(request)
+            print(response)
+            print(data?.description)
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
